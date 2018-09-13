2d74cdc06a000d587e27e5abd081604a14b5a953
judithachieng <judith.achieng@andela.com>
2018-09-14 02:12:33 +0300

    update liskov substitution

--- open-close/index.js
+++ open-close/index.js
@@ -1,15 +1,15 @@
 // **Open/Closed Principle**
 
 // BAD
-var iceCreamFlavors = ["chocolate","vanilla"];
-var iceCreamMaker =  {
-makeIceCream (flavor) {
-  if(iceCreamFlavors.indexOf(flavor)>-1){
-   console.log("Great success. You now have ice cream.");
-  }else{
-   console.log("Epic fail. No ice cream for you.");
-  };
- }
+var iceCreamFlavors = ["chocolate", "vanilla"];
+var iceCreamMaker = {
+    makeIceCream(flavor) {
+        if (iceCreamFlavors.indexOf(flavor) > -1) {
+            console.log("Great success. You now have ice cream.");
+        } else {
+            console.log("Epic fail. No ice cream for you.");
+        };
+    }
 };
 export default iceCreamMaker;
 
@@ -18,14 +18,14 @@ export default iceCreamMaker;
 var iceCreamFlavors = ["chocolate", "vanilla"];
 var iceCreamMaker = {
     makeIceCream(flavor) {
-        if (iceCreamFlavors.indexOf(flavor)>-1) {
+        if (iceCreamFlavors.indexOf(flavor) > -1) {
             console.log("Great success. You now have icecream");
         } else {
             console.log("Epic fail. No ice cream for you.");
         }
     };
 
-    addFlavor (flavor){
+    addFlavor(flavor) {
         iceCreamFlavors.push(flavor);
     }
 };
